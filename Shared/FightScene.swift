2c0615df7e584d258da1e59b930726e1d2b4a0d3
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-08-01 22:26:43 +0200

    added animation.

--- Shared/FightScene.swift
+++ Shared/FightScene.swift
@@ -11,7 +11,6 @@ import SpriteKit
 class FightScene: SKScene {
     var gameState: GameState
     
-    
     init(gameState: GameState) {
         self.gameState = gameState
         super.init(size: CGSize(width: 0, height: 0))
@@ -26,11 +25,17 @@ class FightScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = .red
         
+        let coverSprite = SKSpriteNode(color: .black, size: size)
+        
         let label = SKLabelNode(text: "Fight")
         
         
         anchorPoint = CGPoint(x: 0.5, y: 0.5)
         addChild(label)
+        addChild(coverSprite)
+        
+        let fadeOutAction = SKAction.fadeOut(withDuration: 2)
+        coverSprite.run(fadeOutAction)
     }
     
     override func keyDown(with event: NSEvent) {
