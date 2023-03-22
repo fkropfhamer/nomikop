93d9d3f765530b2845d2f244093fdbaf134e9869
fkropfhamer <fabian.kropfhamer@gmail.com>
2023-03-22 10:46:32 +0100

    :/

--- main.cpp
+++ main.cpp
@@ -2,28 +2,37 @@
 #include <SDL3/SDL.h>
 
 
-
 int main() {
     std::cout << "Bonjour!\n";
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    auto window = SDL_CreateWindow("test", 500, 500, SDL_WINDOW_OPENGL);
+    auto window = SDL_CreateWindow("test", 500, 500, 0);
 
     if (window == NULL) {
         std::cout << "test\n";
+        std::cout << SDL_GetError();
     } else {
         auto screenSurface = SDL_GetWindowSurface(window);
 
-        auto r = SDL_CreateRenderer(window, "ab", SDL_RENDERER_ACCELERATED);
+        auto r = SDL_CreateRenderer(window, NULL, 0);
+        if (r == NULL) {
+            std::cout << "fail\n";
+            std::cout << SDL_GetError();
+        }
 
-        SDL_SetRenderDrawColor(r, 96, 128, 255, 255);
+        auto running = true;
+        auto i = 0;
+        auto flip = true;
+        while (running) {
+            i++;
 
-
-        SDL_RenderPresent(r);
-
-        while (1) {
-            SDL_SetRenderDrawColor(r, 96, 128, 255, 255);
+            if (flip) {
+                SDL_SetRenderDrawColor(r, 0, 255, 0, 255);    
+            } else {
+                SDL_SetRenderDrawColor(r, 0, 0, 255, 255);
+            }
+            
 	        SDL_RenderClear(r);
 
             SDL_Event event;
@@ -31,22 +40,24 @@ int main() {
             while (SDL_PollEvent(&event)) {
                 switch (event.type) {
                     case SDL_EVENT_QUIT:
-                        exit(0);
+                        running = false;
+                        break;
+
+                    case SDL_EVENT_KEY_UP:
+                        std::cout << "key\n";
+                        flip = !flip;
                         break;
                 }
             }
 
             SDL_RenderPresent(r);
-
             SDL_Delay(20);
         }   
     }
     
-    
-
     SDL_DestroyWindow(window);
-
     SDL_Quit();
+
     return 0;
 }
 
